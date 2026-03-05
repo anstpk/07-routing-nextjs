22a6b7dd99e60181967871092d570fcc240cece8
anstpk <anastasiia.stupka@gmail.com>
2026-03-05 18:21:26 +0200

    chore: remove redundant notes/page.tsx as per feedback

--- app/notes/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function NotesRootPage() {
-  // Як тільки користувач потрапляє на /notes, 
-  // ми миттєво відправляємо його на /notes/filter/all
-  redirect('/notes/filter/all');
-}
